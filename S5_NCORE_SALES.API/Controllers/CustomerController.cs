c7290bccd55e03dc9f8ee1b556b80cc92dafb68b
Luis Chang <luis.chang@outlook.com>
2021-09-06 23:42:18 -0500

    Agregar archivos de proyecto.

---
+++ S5_NCORE_SALES.API/Controllers/CustomerController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using S5_NCORE_SALES.CORE.Interfaces;
+using S5_NCORE_SALES.INFRASTRUCTURE.Data;
+using S5_NCORE_SALES.INFRASTRUCTURE.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace S5_NCORE_SALES.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomerController : ControllerBase
+    {
+        private readonly ICustomerRepository _customerRepository;
+
+        public CustomerController(ICustomerRepository customerRepository)
+        {
+            _customerRepository = customerRepository;
+        }
+
+
+        [HttpGet]
+        [Route("Customer")]
+        public async Task<IActionResult> Customer() 
+        {
+            //var customerRepository = new CustomerRepository();
+            var customers = await _customerRepository.GetCustomers();
+            return Ok(customers);
+        }
+
+
+    }
+}
---
+++ S5_NCORE_SALES.INFRASTRUCTURE/Repositories/CustomerRepository.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.EntityFrameworkCore;
+using S5_NCORE_SALES.CORE.Entities;
+using S5_NCORE_SALES.CORE.Interfaces;
+using S5_NCORE_SALES.INFRASTRUCTURE.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace S5_NCORE_SALES.INFRASTRUCTURE.Repositories
+{
+    public class CustomerRepository : ICustomerRepository
+    {
+        private readonly Sales2021Context _context;
+
+        public CustomerRepository(Sales2021Context context)
+        {
+            _context = context;
+        }
+
+        public CustomerRepository()
+        {
+
+        }
+
+        public async Task<IEnumerable<Customer>> GetCustomers()
+        {
+            //using var context = new Sales2021Context();
+            var customers = await _context.Customer.ToListAsync();
+            return customers;
+        }
+
+        public async Task<Customer> GetCustomerById(int id)
+        {
+            //using var context = new Sales2021Context();
+            var customer = await _context.Customer.Where(x => x.Id == id).FirstOrDefaultAsync();
+            return customer;
+        }
+
+        public async Task InsertCustomer(Customer customer)
+        {
+            //using var context = new Sales2021Context();
+            _context.Customer.Add(customer);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task<bool> UpdateCustomer(Customer customer)
+        {
+            //using var context = new Sales2021Context();
+            var customerNow = await _context.Customer.Where(x => x.Id == customer.Id).FirstOrDefaultAsync();
+            customerNow.FirstName = customer.FirstName;
+            customerNow.LastName = customer.LastName;
+            customerNow.City = customer.City;
+            customerNow.Phone = customer.Phone;
+            customerNow.Country = customer.Country;
+
+            int countRows = await _context.SaveChangesAsync();
+            return (countRows > 0);
+        }
+
+        public async Task<bool> DeleteCustomer(int id)
+        {
+            //using var context = new Sales2021Context();
+            var customerNow = await _context.Customer.Where(x => x.Id == id).FirstOrDefaultAsync();
+            _context.Customer.Remove(customerNow);
+            int countRows = await _context.SaveChangesAsync();
+            return (countRows > 0);
+        }
+
+
+
+    }
+}
